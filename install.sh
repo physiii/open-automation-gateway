cfa2006a52ed3d19e851c8d3ea767974144713ab
physiii <andy@pyfi.org>
2017-07-14 09:11:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,12 +13,13 @@
 sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get update
+sudo rpi-source
 
 sudo apt-get install -y --force-yes \
 bc g++ lua5.2 bc sshpass libudev-dev nmap motion speedtest-cli gstreamer1.0 \
 v4l2loopback-dkms v4l2loopback-utils git nodejs mongodb dnsmasq hostapd \
 pkg-config libudev-dev libjpeg-dev libavformat-dev libavcodec-dev libavutil-dev \
-libc6-dev zlib1g-dev libpq5 libpq-dev tmux xdotool
+libc6-dev zlib1g-dev libpq5 libpq-dev tmux xdotool libncurses5-dev
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo chmod a+rwxt /var/run/motion
