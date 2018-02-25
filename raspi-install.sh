fd17b6f7717b76ca07032e64e3e29123ab812e58
physiii <andy@pyfi.org>
2018-02-25 17:30:49 -0600

    Update raspi-install.sh

--- raspi-install.sh
+++ raspi-install.sh
@@ -21,6 +21,29 @@ sudo easy_install pip
 sudo python -m pip install pymongo==3.0.3 numpy imutils opencv-python
 sudo npm install -g pm2
 
+## opencv
+
+#nano /etc/dphys-swapfile
+#CONF_SWAPSIZE=1024
+#sudo /etc/init.d/dphys-swapfile stop
+#sudo /etc/init.d/dphys-swapfile start
+cd ~
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+unzip opencv.zip
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.0.zip
+unzip opencv_contrib.zip
+cd ~/opencv-3.3.0/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D INSTALL_PYTHON_EXAMPLES=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
+      -D BUILD_EXAMPLES=ON ..
+make -j4
+sudo make install
+sudo ldconfig
+
 ## make and install openzwave
 cd /usr/src
 wget http://old.openzwave.com/downloads/openzwave-1.4.1.tar.gz
