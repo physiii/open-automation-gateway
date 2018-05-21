5870648c7986f386f6b51df3ef45cb642f902f82
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-05-21 09:49:19 -0500

    Forming groundwork for the Thermostat service class.

--- services/thermostat-service.js
+++ services/thermostat-service.js
@@ -1,1 +1,20 @@
 //Placeholder for Thermostat service
+const Service = require('./service.js'),
+  TAG = '[ThermostatService]';
+
+  class ThermostatService extends Service {
+    constructor(data) { // TODO: Add driverClass arguement when ready
+      super(data);
+
+      this.id = data.id;
+      this.settings.systen_info = data.settings && data.settings.system_info;
+      this.settings.program_info = data.settings && data.settings.program_info;
+      this.mode = data.mode; // Determines Heating or Cooling
+      this.fan_mode = data.fan_mode; // Determines whether the fan is on or off
+      this.target_temp = data.target_temp;
+      this.current_temp = data.current_temp;
+
+      //this.driver = new driverClass(this.id);
+      //this.subscribeToDriver();
+    }
+  }
