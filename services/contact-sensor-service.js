91290935284c09ef1dea67583eb552bd9806ec81
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-11-07 16:51:52 -0600

    modified GPIO pin used

--- services/contact-sensor-service.js
+++ services/contact-sensor-service.js
@@ -1,6 +1,6 @@
 const Service = require('./service.js'),
   Gpio = require('onoff').Gpio,
-  Sensor = new Gpio(21, 'in', 'both'),
+  Sensor = new Gpio(4, 'in', 'both'),
 	ContactSensorApi = require('./api/contact-sensor-api.js'),
 	TAG = '[ContactSensorService]';
 
@@ -10,20 +10,7 @@ class ContactSensorService extends Service {
 
 		this.ip = data.ip;
 
-    Sensor.watch((err, value) => {
-      if (err) {
-        throw err;
-      }
-
-      if (value === 0) {
-        this.state.contact = false;
-      } else if (value === 1) {
-        this.state.contact = true;
-      } else {
-        console.log(TAG, 'Value from contact sensor GPIO  invalid');
-      }
-
-    });
+    this.startSensor();
 	}
 
 	subscribeToDriver () {
@@ -33,6 +20,29 @@ class ContactSensorService extends Service {
 	onReady (data) {
 		return;
 	}
+
+  startSensor () {
+    Sensor.watch((err, value) => {
+      if (err) {
+        throw err;
+      }
+
+      if (value === 0) {
+        console.log(TAG, 'Contact Sensor not connected');
+        this.state.contact = false;
+      } else if (value === 1) {
+        console.log(TAG, 'Contact Sensor connected');
+        this.state.contact = true;
+      } else {
+        console.log(TAG, 'Value from contact sensor GPIO  invalid');
+      }
+
+    });
+
+    process.on('SIGINT', () => {
+      Sensor.unexport();
+    });
+  }
 
 	dbSerialize () {
 		return {
