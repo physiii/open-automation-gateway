3466f2fde36c70f72cbf31453daac284b1ec5aa5
physiii <andy@pyfi.org>
2019-01-03 13:38:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,51 @@
 # sudo apt upgrade -y
 # sudo rpi-update
 
+
+############
+## opencv ##
+############
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install -y build-essential cmake pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk2.0-dev libgtk-3-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y python2.7-dev python3-dev
+
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran python2.7-dev python3-dev
+
+cd ~
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+unzip opencv.zip
+
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.0.zip
+unzip opencv_contrib.zip
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+
+pip install numpy
+
+cd ~/opencv-3.3.0/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
+    -D BUILD_EXAMPLES=ON ..
+ 
+make -j4
+sudo make install
+sudo ldconfig
+
+############
+##  node  ##
+############
+
 sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - 
 
