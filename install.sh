f6d416a8629fcd06f6f19213bce764bb761bd701
Andy Payne <andy@pyfi.org>
2019-02-21 13:00:39 -0600

    update

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 sudo apt-get install -y \
   sshpass git nodejs mongodb dnsmasq hostapd tmux xdotool libudev-dev \
-  python-pip python-setuptools python-dev python2.7-dev python-opencv \
-  python-setuptools libssl-dev libasound2-dev raspberrypi-kernel-headers \
+  python-pip python-setuptools python-dev python2.7-dev \
+  libssl-dev libasound2-dev raspberrypi-kernel-headers nmap \
   build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev \
   libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev \
   libatlas-base-dev gfortran python3-dev libavcodec-dev libavformat-dev
@@ -44,41 +44,24 @@ sudo make install
 # sudo /etc/init.d/dphys-swapfile start
 
 cd /usr/local/src
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.0.zip
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
 unzip opencv.zip
 unzip opencv_contrib.zip
 
-cd opencv-3.3.0
+cd opencv-4.0.0
 mkdir build
 cd build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D BUILD_WITH_DEBUG_INFO=OFF \
-	-D BUILD_DOCS=OFF \
-	-D BUILD_EXAMPLES=OFF \
-	-D BUILD_TESTS=OFF \
-	-D BUILD_opencv_ts=OFF \
-	-D BUILD_PERF_TESTS=OFF \
-	-D INSTALL_C_EXAMPLES=OFF \
-	-D INSTALL_PYTHON_EXAMPLES=OFF \
-	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.3.0/modules \
-	-D ENABLE_NEON=ON \
-  -D ENABLE_VFPV3=ON \
-	-D WITH_LIBV4L=ON \
-	-D WITH_FFMPEG=OFF \
-  .. \
-
-# cmake -D CMAKE_BUILD_TYPE=RELEASE \
-#    -D CMAKE_INSTALL_PREFIX=/usr/local \
-#    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.3.0/modules \
-#    -D ENABLE_NEON=ON \
-#    -D ENABLE_VFPV3=ON \
-#    -D BUILD_TESTS=OFF \
-#    -D INSTALL_PYTHON_EXAMPLES=OFF \
-#    -D BUILD_EXAMPLES=OFF \
-#    .. \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.0.0/modules \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D BUILD_EXAMPLES=OFF ..
 
 make -j4
 sudo make install
@@ -101,6 +84,7 @@ sudo ln -s /usr/local/lib64/libopenzwave.so.1.4 /usr/local/lib/
 ######################
 ##  video loopback  ##
 ######################
+#sudo ln -s /lib/modules/4.14.98-v7+ /lib/modules/4.14.79-v7+
 
 sudo chown -R $USER /usr/src
 cd /usr/src
@@ -116,7 +100,7 @@ sudo modprobe v4l2loopback video_nr=10,20
 
 export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
 cd /usr/local/src
-git clone https://github.com/physiii/open-automation-gateway gateway
+git clone https://github.com/physiii/open-automation-gateway -b dev gateway
 cd gateway
 npm install
 sudo chmod 777 /etc/wpa_supplicant/wpa_supplicant.conf
