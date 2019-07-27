40b14ca239304ed09fc68d1d01c1177a9f266b88
physiii <andy@pyfi.org>
2019-07-27 17:19:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -86,6 +86,9 @@ sudo ln -s /usr/local/lib64/libopenzwave.so.1.4 /usr/local/lib/
 ######################
 #sudo ln -s /lib/modules/4.14.98-v7+ /lib/modules/4.14.79-v7+
 #sudo ln -s /usr/src/linux-headers-4.14.98-v7+ /lib/modules/4.14.98-v7+/build
+#sudo make KERNELRELEASE=4.14.98-v7+
+#sudo make install KERNELRELEASE=4.14.98-v7+
+
 
 sudo chown -R $USER /usr/src
 cd /usr/src
