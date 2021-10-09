072566117f5b0e3d2fa9b29a1989e87e0bd9ea82
physiii <andy@pyfi.org>
2021-10-09 10:04:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,6 +64,32 @@ make -j4
 sudo make install
 sudo ldconfig
 
+## Ubuntu
+sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev \
+libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+libtbb2 libtbb-dev libdc1394-22-dev libopenexr-dev \
+libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+
+mkdir ~/opencv_build && cd ~/opencv_build
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+
+cd ~/opencv_build/opencv
+mkdir -p build && cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D INSTALL_C_EXAMPLES=ON \
+-D INSTALL_PYTHON_EXAMPLES=ON \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules \
+-D BUILD_EXAMPLES=ON ..
+
+make -j8
+sudo make install
+
 #################
 ##  openzwave  ##
 #################
